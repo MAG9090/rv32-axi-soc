0103d2ff201159bbf283579caf5c0556fdcedfb3
Wei <xwei308@ece-rschsrv.ece.gatech.edu>
2026-03-15 22:27:12 -0400

    update setup_edu.sh commands

--- setup_eda.sh
+++ setup_eda.sh
@@ -2,8 +2,8 @@
 # ============================================
 # EDA Tool Setup Script
 # ============================================
-# University: [Your University]
-# Server:     [Your Server Name]
+# University: Goergia Tech
+# Server:     ece-rschsrv.ece.gatech.edu
 # 
 # Usage: source setup_eda.sh
 # Note:  Must be sourced, not executed (need environment changes in current shell)
@@ -16,20 +16,18 @@ module load ece-software
 # Synopsys Tools
 # --------------------------------------------
 # Design Compiler (synthesis)
-source /tools/software/synopsys/syn/latest/bin/environ.sh
+# source /tools/software/synopsys/syn/latest/bin/environ.sh
 
 # VCS (simulation)
-source /tools/software/synopsys/vcs/latest/bin/environ.sh
+# ource /tools/software/synopsys/vcs/latest/bin/environ.sh
 
 # Verdi (waveform debug)
-source /tools/software/synopsys/verdi/latest/bin/environ.sh
+# source /tools/software/synopsys/verdi/latest/bin/environ.sh
 
 # PrimeTime (static timing analysis)
-# Uncomment and update path once confirmed:
 # source /tools/software/synopsys/prime/latest/bin/environ.sh
 
 # SpyGlass (linting / CDC)
-# Uncomment and update path once confirmed:
 # source /tools/software/synopsys/spyglass/latest/bin/environ.sh
 
 # --------------------------------------------
@@ -45,7 +43,7 @@ module load siemens/latest
 # --------------------------------------------
 # Xilinx Vivado (FPGA)
 # --------------------------------------------
-module load xilinx/2023.1
+module load xilinx/latest
 
 # --------------------------------------------
 # Verify tools loaded
